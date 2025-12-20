e882c4c16ede964b684196a708e05955e5919839
cataclysm <rage.cataclysm@gmail.com>
2025-12-20 16:55:38 +0630

    added create method for both Administrator and Role

--- projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/service/AdministratorService.java
+++ projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/service/AdministratorService.java
@@ -4,13 +4,27 @@ import com.tamantaw.projectx.persistence.criteria.AdministratorCriteria;
 import com.tamantaw.projectx.persistence.dto.AdministratorDTO;
 import com.tamantaw.projectx.persistence.entity.Administrator;
 import com.tamantaw.projectx.persistence.entity.QAdministrator;
+import com.tamantaw.projectx.persistence.entity.Role;
+import com.tamantaw.projectx.persistence.entity.AdministratorRole;
+import com.tamantaw.projectx.persistence.exception.ConsistencyViolationException;
+import com.tamantaw.projectx.persistence.exception.PersistenceException;
 import com.tamantaw.projectx.persistence.mapper.AdministratorMapper;
 import com.tamantaw.projectx.persistence.repository.AdministratorRepository;
 import com.tamantaw.projectx.persistence.service.base.BaseService;
+import jakarta.persistence.EntityManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.tamantaw.projectx.persistence.utils.LoggerConstants.DATA_INTEGRITY_VIOLATION_MSG;
 
 @Service
 public class AdministratorService
@@ -24,11 +38,70 @@ public class AdministratorService
 	private static final Logger log =
 			LogManager.getLogger("serviceLogs." + AdministratorService.class.getSimpleName());
 
+	private final EntityManager entityManager;
+
 	@Autowired
 	public AdministratorService(
 			AdministratorRepository administratorRepository,
-			AdministratorMapper mapper
+			AdministratorMapper mapper,
+			EntityManager entityManager
 	) {
 		super(administratorRepository, mapper);
+		this.entityManager = entityManager;
+	}
+
+	public Administrator create(AdministratorDTO dto, List<Long> roleIds, long createdBy)
+			throws PersistenceException, ConsistencyViolationException {
+
+		Assert.notNull(dto, "DTO must not be null");
+		Assert.notNull(roleIds, "roleIds must not be null");
+		Assert.noNullElements(roleIds, "roleIds must not contain null elements");
+
+		String c = String.format(
+				"[service=%s][dto=%s]",
+				serviceName(),
+				dto.getClass().getSimpleName()
+		);
+
+		log.info("{} CREATE_WITH_ROLES start createdBy={} roleCount={}",
+				c, createdBy, roleIds.size());
+
+		try {
+			Administrator entity = mapper.toEntity(dto);
+			entity.setCreatedBy(createdBy);
+			entity.setUpdatedBy(createdBy);
+
+			Set<Long> uniqueRoleIds = new LinkedHashSet<>(roleIds);
+			List<AdministratorRole> administratorRoles = new ArrayList<>(uniqueRoleIds.size());
+
+			for (Long roleId : uniqueRoleIds) {
+				AdministratorRole administratorRole = new AdministratorRole();
+				administratorRole.setAdministrator(entity);
+				administratorRole.setRole(entityManager.getReference(Role.class, roleId));
+				administratorRole.setCreatedBy(createdBy);
+				administratorRole.setUpdatedBy(createdBy);
+				administratorRoles.add(administratorRole);
+			}
+
+			entity.setAdministratorRoles(administratorRoles);
+
+			Administrator saved = repository.saveRecord(entity);
+
+			log.info("{} CREATE_WITH_ROLES success id={} roles={}",
+					c, saved.getId(), administratorRoles.size());
+			return saved;
+		}
+		catch (DataIntegrityViolationException e) {
+			log.error("{} CREATE_WITH_ROLES integrity violation dto={} roles={}",
+					c, dto, roleIds, e);
+			throw new ConsistencyViolationException(DATA_INTEGRITY_VIOLATION_MSG, e);
+		}
+		catch (Exception e) {
+			log.error("{} CREATE_WITH_ROLES failed dto={} roles={}", c, dto, roleIds, e);
+			throw new PersistenceException(
+					"CreateWithRoles failed dto=" + dto.getClass().getSimpleName(),
+					e
+			);
+		}
 	}
 }
--- projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/service/RoleService.java
+++ projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/service/RoleService.java
@@ -2,15 +2,29 @@ package com.tamantaw.projectx.persistence.service;
 
 import com.tamantaw.projectx.persistence.criteria.RoleCriteria;
 import com.tamantaw.projectx.persistence.dto.RoleDTO;
+import com.tamantaw.projectx.persistence.entity.Action;
 import com.tamantaw.projectx.persistence.entity.QRole;
 import com.tamantaw.projectx.persistence.entity.Role;
+import com.tamantaw.projectx.persistence.entity.RoleAction;
+import com.tamantaw.projectx.persistence.exception.ConsistencyViolationException;
+import com.tamantaw.projectx.persistence.exception.PersistenceException;
 import com.tamantaw.projectx.persistence.mapper.RoleMapper;
 import com.tamantaw.projectx.persistence.repository.RoleRepository;
 import com.tamantaw.projectx.persistence.service.base.BaseService;
+import jakarta.persistence.EntityManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.tamantaw.projectx.persistence.utils.LoggerConstants.DATA_INTEGRITY_VIOLATION_MSG;
 
 @Service
 public class RoleService
@@ -24,10 +38,67 @@ public class RoleService
 	private static final Logger log =
 			LogManager.getLogger("serviceLogs." + RoleService.class.getSimpleName());
 
+	private final EntityManager entityManager;
+
 	@Autowired
-	public RoleService(RoleRepository roleRepository, RoleMapper mapper) {
+	public RoleService(RoleRepository roleRepository, RoleMapper mapper, EntityManager entityManager) {
 
 		super(roleRepository, mapper);
+		this.entityManager = entityManager;
+	}
+
+	public Role create(RoleDTO dto, List<Long> actionIds, long createdBy)
+			throws PersistenceException, ConsistencyViolationException {
+
+		Assert.notNull(dto, "DTO must not be null");
+		Assert.notNull(actionIds, "actionIds must not be null");
+		Assert.noNullElements(actionIds, "actionIds must not contain null elements");
+
+		String c = String.format(
+				"[service=%s][dto=%s]",
+				serviceName(),
+				dto.getClass().getSimpleName()
+		);
+
+		log.info("{} CREATE_WITH_ACTIONS start createdBy={} actionCount={}",
+				c, createdBy, actionIds.size());
+
+		try {
+			Role entity = mapper.toEntity(dto);
+			entity.setCreatedBy(createdBy);
+			entity.setUpdatedBy(createdBy);
+
+			Set<Long> uniqueActionIds = new LinkedHashSet<>(actionIds);
+			List<RoleAction> roleActions = new ArrayList<>(uniqueActionIds.size());
+
+			for (Long actionId : uniqueActionIds) {
+				RoleAction roleAction = new RoleAction();
+				roleAction.setRole(entity);
+				roleAction.setAction(entityManager.getReference(Action.class, actionId));
+				roleAction.setCreatedBy(createdBy);
+				roleAction.setUpdatedBy(createdBy);
+				roleActions.add(roleAction);
+			}
+
+			entity.setRoleActions(roleActions);
+
+			Role saved = repository.saveRecord(entity);
+
+			log.info("{} CREATE_WITH_ACTIONS success id={} actions={}",
+					c, saved.getId(), roleActions.size());
+			return saved;
+		}
+		catch (DataIntegrityViolationException e) {
+			log.error("{} CREATE_WITH_ACTIONS integrity violation dto={} actions={}",
+					c, dto, actionIds, e);
+			throw new ConsistencyViolationException(DATA_INTEGRITY_VIOLATION_MSG, e);
+		}
+		catch (Exception e) {
+			log.error("{} CREATE_WITH_ACTIONS failed dto={} actions={}", c, dto, actionIds, e);
+			throw new PersistenceException(
+					"CreateWithActions failed dto=" + dto.getClass().getSimpleName(),
+					e
+			);
+		}
 	}
 }
-
