cf94c77f77b7e5f6f088b659126dce26e866a599
cataclysm <rage.cataclysm@gmail.com>
2025-12-21 16:40:36 +0630

    temporarily disable PersistenceProfiler

--- projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/config/PersistenceProfiler.java
+++ projectx-persistence/src/main/java/com/tamantaw/projectx/persistence/config/PersistenceProfiler.java
@@ -1,25 +1,20 @@
 package com.tamantaw.projectx.persistence.config;
 
-import com.tamantaw.projectx.persistence.utils.YamlPropertySourceFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
+//@Configuration
 public class PersistenceProfiler {
 
-	@Configuration
-	@Profile("default")
-	@PropertySource(value = "classpath:persistence-dev.yml", factory = YamlPropertySourceFactory.class)
-	static class Default {}
-
-	@Configuration
-	@Profile("dev")
-	@PropertySource(value = "classpath:persistence-dev.yml", factory = YamlPropertySourceFactory.class)
-	static class ReportingPlatformDevelopment {}
-
-	@Configuration
-	@Profile("prd")
-	@PropertySource(value = "classpath:persistence-prd.yml", factory = YamlPropertySourceFactory.class)
-	static class ReportingPlatformProduction {}
+//	@Configuration
+//	@Profile("default")
+//	@PropertySource(value = "classpath:persistence-dev.yml", factory = YamlPropertySourceFactory.class)
+//	static class Default {}
+//
+//	@Configuration
+//	@Profile("dev")
+//	@PropertySource(value = "classpath:persistence-dev.yml", factory = YamlPropertySourceFactory.class)
+//	static class ReportingPlatformDevelopment {}
+//
+//	@Configuration
+//	@Profile("prd")
+//	@PropertySource(value = "classpath:persistence-prd.yml", factory = YamlPropertySourceFactory.class)
+//	static class ReportingPlatformProduction {}
 }
